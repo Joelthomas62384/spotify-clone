9d59b5f62b1fbcb265771574305e30da97a52499
Joel Thomas <joelthomas1103@gmail.com>
2024-06-19 21:02:40 +0530

    done

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ document.getElementById('hero').addEventListener('scroll', function() {
     if(scrollPosition>0){
         // navbar.style.transitionDelay = 
         navbar.style.backgroundColor = "rgb(18, 18, 18)"
-        if(scrollPosition>200){
+        
             hero.style.backgroundImage = "linear-gradient(to bottom, #1b1a1aca  0%, rgba(57, 57, 57, 0.237) 60%, rgb(0, 0, 0) 100%)"
 
-    }
+    
 }else{
     
     hero.style.backgroundImage = "linear-gradient(to bottom, #302f2fce 0%, rgba(0, 0, 0, 0.527) 60%, rgb(0, 0, 0) 100%)"
